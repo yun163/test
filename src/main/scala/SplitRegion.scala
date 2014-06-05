0507651af65585df8a0c522ebea1715ff1f45891
liweichao <weichao092@163.com>
2014-06-06 00:29:49 +0800

    remove performance test

--- src/main/scala/SplitRegion.scala
+++ src/main/scala/SplitRegion.scala
@@ -7,19 +7,13 @@ import org.apache.hadoop.hbase.HBaseConfiguration
 object SplitRegion extends App {
 
   override def main(args: Array[String]) = {
-    create(args)
-//    getSplitKeys(18, true)
+//    create(args)
+    split(args)
+//    getSplitKeys(3, true)
   }
 
-  def create(args: Array[String]) = {
-    if (args.length != 5) {
-      println("""enter 5 parameters for "cluster.distributed[1: true, 0: false]", "zookeeper.quorum[hadoop:2181]",  "table[messages]", "family[a]", "splitCount[18]" """)
-      sys.exit(-1)
-    }
-    val conf = HBaseConfiguration.create()
-    conf.set("cluster.distributed", if ("1".equals(args(0))) "true" else "false")
-    conf.set("zookeeper.quorum", args(1))
-    val admin = new HBaseAdmin(conf)
+  private def create(args: Array[String]) = {
+    val admin = getAdmin(args)
     val tableBytes = Bytes.toBytes(args(2))
     val familyBytes = Bytes.toBytes(args(3))
     val splitNum = Integer.parseInt(args(4))
@@ -38,12 +32,40 @@ object SplitRegion extends App {
       val familyDesc = genColumnFamily(familyBytes, 1)
       tableDesc.addFamily(familyDesc)
       if (splitNum > 1) {
-        val splitPoints = getSplitKeys(splitNum);
+        val splitPoints = getSplitKeys(splitNum)
         admin.createTable(tableDesc, splitPoints)
       } else {
         admin.createTable(tableDesc)
       }
     }
+    admin.close()
+  }
+
+  private def split(args: Array[String]) {
+    val admin = getAdmin(args)
+    val tableBytes = Bytes.toBytes(args(2))
+    if (!admin.tableExists(tableBytes)) {
+      sys.error(s"Table not exists for ${args(2)}")
+    }
+    val splitNum = Integer.parseInt(args(4))
+    val splitPoints = getSplitKeys(splitNum)
+    splitPoints foreach {
+      point =>
+        admin.split(tableBytes, point)
+        Thread.sleep(90000)
+    }
+    admin.close()
+  }
+
+  private def getAdmin(args: Array[String]):HBaseAdmin = {
+    if (args.length != 5) {
+      println("""enter 5 parameters for "cluster.distributed[1: true, 0: false]", "zookeeper.quorum[hadoop:2181]",  "table[messages]", "family[a]", "splitCount[18]" """)
+      sys.exit(-1)
+    }
+    val conf = HBaseConfiguration.create()
+    conf.set("cluster.distributed", if ("1".equals(args(0))) "true" else "false")
+    conf.set("zookeeper.quorum", args(1))
+    new HBaseAdmin(conf)
   }
 
   private def genColumnFamily(family: Array[Byte],  columnMaxVersion: Int): HColumnDescriptor = {
@@ -69,9 +91,9 @@ object SplitRegion extends App {
     list.toArray
   }
 
-  def padNum(l: Int, howLong: Int):String = String.valueOf(l).reverse.padTo(howLong, "0").reverse.mkString.substring(0, howLong)
+  private def padNum(l: Int, howLong: Int):String = String.valueOf(l).reverse.padTo(howLong, "0").reverse.mkString.substring(0, howLong)
 
-  def renderBytes(bytes: Array[Byte]): String = {
+  private def renderBytes(bytes: Array[Byte]): String = {
     bytes.map("%02x".format(_)).mkString
   }
 }
